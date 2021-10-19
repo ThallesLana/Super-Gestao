3dd0580ea3b465b0dd2d3dbfb32cecc94ba31861
ThallesLana <thalles.lana1@gmail.com>
2021-10-18 23:26:29 -0300

    Study parameters in routes

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,14 @@ Route::get('/', 'PrincipalController@principal');
 Route::get('/sobre-nos', 'SobrenosController@sobrenos');
 
 Route::get('/contato','ContatoController@contato');
+// nome, categoria, assunto e mensagem
+
+// espera de parametros por rota
+// ? significa que o parametro é opcional
+Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
+    function(string $nome = 'Desconhecido', $categoria = 'Informação', $assunto = 'Contato', $mensagem = 'mensagem não informada') {
+    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+});
 
 // O primeiro a ser declarado é o caminho, o segundo é o callback que define o direcionamento
 // Route::get($uri, $callback);
